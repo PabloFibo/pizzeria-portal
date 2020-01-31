cfc8dc74f23d527d50c89147c0bbf056a4a7b72d
Paweł Hoła <phola85@wp.pl>
2020-01-31 21:31:14 +0100

    Add Material-UI for app routing view

--- src/App.js
+++ src/App.js
@@ -7,19 +7,33 @@ import Login from './components/views/Login/Login';
 import Tables from './components/views/Tables/Tables';
 import WaiterView from './components/views/WaiterView/WaiterView';
 import KitchenView from './components/views/KitchenView/KitchenView';
+import { StylesProvider } from '@material-ui/core/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: '#2b4c6f',
+    },
+  },
+});
 
 function App() {
   return (
     <BrowserRouter>
-      <MainLayout>
-        <Switch>
-          <Route exact path={`${process.env.PUBLIC_URL}/`} component={Homepage}/>
-          <Route path={`${process.env.PUBLIC_URL}/login`} component={Login}/>
-          <Route path={`${process.env.PUBLIC_URL}/tables`} component={Tables}/>
-          <Route path={`${process.env.PUBLIC_URL}/waiter`} component={WaiterView}/>
-          <Route path={`${process.env.PUBLIC_URL}/kitchen`} component={KitchenView}/>
-        </Switch>
-      </MainLayout>
+      <StylesProvider injectFirst>
+        <ThemeProvider theme={theme}>
+          <MainLayout>
+            <Switch>
+              <Route exact path={`${process.env.PUBLIC_URL}/`} component={Homepage}/>
+              <Route path={`${process.env.PUBLIC_URL}/login`} component={Login}/>
+              <Route path={`${process.env.PUBLIC_URL}/tables`} component={Tables}/>
+              <Route path={`${process.env.PUBLIC_URL}/waiter`} component={WaiterView}/>
+              <Route path={`${process.env.PUBLIC_URL}/kitchen`} component={KitchenView}/>
+            </Switch>
+          </MainLayout>
+        </ThemeProvider>
+      </StylesProvider>
     </BrowserRouter>
   );
 }
