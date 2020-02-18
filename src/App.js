4cb9564e868ad75db66f2f296b5405dfbe4652ce
Paweł Hoła <phola85@wp.pl>
2020-02-18 23:03:06 +0100

    Add thunk creators and axios for connection with API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import MainLayout from './components/layout/MainLayout/MainLayout';
 import Homepage from './components/views/Homepage/Homepage';
@@ -7,6 +8,7 @@ import Login from './components/views/Login/Login';
 import Tables from './components/views/Tables/Tables';
 import WaiterView from './components/views/WaiterView/WaiterView';
 import KitchenView from './components/views/KitchenView/KitchenView';
+import store from './redux/store.js';
 import { StylesProvider } from '@material-ui/core/styles';
 import { createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
@@ -20,21 +22,23 @@ const theme = createMuiTheme({
 
 function App() {
   return (
-    <BrowserRouter>
-      <StylesProvider injectFirst>
-        <ThemeProvider theme={theme}>
-          <MainLayout>
-            <Switch>
-              <Route exact path={`${process.env.PUBLIC_URL}/`} component={Homepage}/>
-              <Route path={`${process.env.PUBLIC_URL}/login`} component={Login}/>
-              <Route path={`${process.env.PUBLIC_URL}/tables`} component={Tables}/>
-              <Route path={`${process.env.PUBLIC_URL}/waiter`} component={WaiterView}/>
-              <Route path={`${process.env.PUBLIC_URL}/kitchen`} component={KitchenView}/>
-            </Switch>
-          </MainLayout>
-        </ThemeProvider>
-      </StylesProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <StylesProvider injectFirst>
+          <ThemeProvider theme={theme}>
+            <MainLayout>
+              <Switch>
+                <Route exact path={`${process.env.PUBLIC_URL}/`} component={Homepage}/>
+                <Route path={`${process.env.PUBLIC_URL}/login`} component={Login}/>
+                <Route path={`${process.env.PUBLIC_URL}/tables`} component={Tables}/>
+                <Route path={`${process.env.PUBLIC_URL}/waiter`} component={WaiterView}/>
+                <Route path={`${process.env.PUBLIC_URL}/kitchen`} component={KitchenView}/>
+              </Switch>
+            </MainLayout>
+          </ThemeProvider>
+        </StylesProvider>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
